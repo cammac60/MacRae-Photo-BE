962a0179a42850f939ab0a6aea0a6863365d7459
Cameron MacRae <cam.mac.cm@gmail.com>
2020-03-11 20:44:17 -0600

    Add tests

--- app.test.js
+++ app.test.js
@@ -11,5 +11,23 @@ describe('Server', () => {
     await database.seed.run();
   });
 
-  
-};
+  describe('POST /api/v1/messages', () => {
+
+    it('Should return a 201 status and the message id', async () => {
+      const newMessage = {email: 'newEmail', body: 'newMessage'};
+      const response = await request(app).post('/api/v1/messages').send(newMessage);
+      const messages = await database('messages').where('id', response.body.id);
+      const message = messages[0];
+      expect(response.status).toBe(201);
+      expect(message.body).toEqual(newMessage.body);
+    });
+
+    it('Should return a 422 status and an error message if a name property is not included in the request body', async () => {
+      const response = await request(app).post('/api/v1/messages').send({});
+      expect(response.status).toBe(422);
+      expect(response.body.error).toEqual('Expected body format is: { email: <String>, body: <String>}. You\'re missing the required "email" property.');
+    });
+
+  });
+
+});
