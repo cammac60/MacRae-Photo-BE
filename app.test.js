dd3d68a19c0b2f42d77108d29a492a7d10efdc77
Cameron MacRae <cam.mac.cm@gmail.com>
2020-03-11 20:31:02 -0600

    Setup test suite

---
+++ app.test.js
@@ -0,0 +1,15 @@
+const request = require('supertest');
+const app = require('./app.js');
+
+const environment = process.env.NODE_ENV || 'development'
+const configuration = require('./knexfile')[environment]
+const database = require('knex')(configuration)
+
+describe('Server', () => {
+
+  beforeEach(async () => {
+    await database.seed.run();
+  });
+
+  
+};
